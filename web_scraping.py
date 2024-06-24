ac3bade7323d385cc799d30072c6125b7a9f02c9
Maredol <b435924@fgv.edu.br>
2024-06-24 15:40:04 -0300

    finalização da formatação do arq web_scraping.py com auxílio do arq formating_web_scraping.py
    principais mudanças:
    adicionadas funções para separação e limpeza das listas:
    lançamento, bilheteria, genero, diretor
    adicionada função de chamar main: call_main()
    tranferência do codigo final de formating_web_scraping.py para web_scraping.py e subsequente delete do arq formating_web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,160 +1,191 @@
-"""Web Scraping do site de avaliações de filmes Rotten Tomatoes"""
-
-# aqui são importadas as bibliotecas que serão utilizadas para extrair os dados
+#importando as bibliotecas necessárias
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Scraping da pagina principal
-url = "https://www.rottentomatoes.com/browse/movies_in_theaters/"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "lxml")
+#scraping de paginas
+def scraping(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "lxml")
+    return soup
 
-# Listas com os dados extraidos
-titulos = []
-lancamentos = []
-generos = []
-diretores = []
-sinopses = []
-bilheterias = []
-# Porcentagem de avaliações dos críticos
-tomatometer_scores = []
-# Porcentagem de avaliações do público
-audience_scores = []
-# Número de avaliações dos críticos
-number_scores = []
-# Consenso dos críticos
-critics_consensus = []
-# Consenso do público
-audience_consensus = []
+#inicializando as variavies do tipo lista
+def declarar_listas():
+    titulos = []
+    lancamentos = []
+    generos = []
+    diretores = []
+    sinopses = []
+    bilheterias = []
+    tomatometer_scores = []
+    audience_scores = []
+    number_scores = []
+    critics_consensus = []
+    audience_consensus = []
+    links_filmes = []
+    
+    lista = titulos, lancamentos, generos, diretores,\
+        sinopses, bilheterias, tomatometer_scores,\
+        audience_scores, number_scores, critics_consensus,\
+        audience_consensus, links_filmes
+        
+    return lista
 
-# Identificar os links de redirecionamento para cada filme da pagina
-# Obs: precisamos dividir em duas partes pois o link de alguns filmes estava em um atributo diferente
-links_filmes = []
-filmes_elements = soup.find_all("a", class_="js-tile-link")
-filmes_elements2 = soup.find_all("a", attrs={'data-track': 'scores'})
+#encontrando a terminação dos links
+def encontrar_link():
+    links_filmes = lista[11]
+    filmes_elements = soup.find_all("a", class_="js-tile-link") + soup.find_all("a", attrs={'data-track': 'scores'})
 
-# Adicionando os links na lista
-# Obs: esses links são apenas a terminação que é adicionada ao endereço principal da pagina
-for element in filmes_elements:
-    links_filmes.append(element['href'])
-    
-for element in filmes_elements2:
-    links_filmes.append(element['href'])
-    
-# O filme_url contém o endereço da pagina principal mais a terminação que redireciona para um filme especifico
-for link_filme in links_filmes:
-    filme_url = "https://www.rottentomatoes.com" + link_filme
-    filme_response = requests.get(filme_url)
-    filme_soup = BeautifulSoup(filme_response.text, 'lxml')
-    
-    # Encontrando o nome do filme
-    titulo = filme_soup.find("h1", class_="unset").find('span').text.strip()
-    
-    # A sinopse está dentro de <rt-text> que está dentro de uma div que contém dois <rt-text>, mas so queremos o segundo
-    sinopse_element = filme_soup.find("div", class_="synopsis-wrap")
-    if sinopse_element:
-        sinopse_rt_texts = sinopse_element.find_all('rt-text')
-        if len(sinopse_rt_texts) > 1:
-            sinopse = sinopse_rt_texts[1].text.strip()
-    
-    # Encontrando informações básicas do filme que são fornecidas em uma lista, ou seja, as informações estão em um mesmo tipo de elemento dt e dd
-    # precisamos então diferencia-las pelo texto
+    for element in filmes_elements:
+        links_filmes.append(element['href']) 
+        
+    return links_filmes
+
+#limpando os dados do scraping e separando-os em cada uma de suas listas
+#1° -> Critics/Audience Scores, Consensus e Reviews
+def separar_critics_audience(filme_arg, string, strain, element_arg, posicao):
+    info = lista[posicao]
+    element = filme_soup.find(f"{filme_arg}", attrs={f"{string}" : f"{strain}"})
+    if element and len(element_arg) >= 1:
+        text = element.find(f"{element_arg}")
+        if text:
+            info = text.text.strip()
+            if not info:
+                info = "nd%"
+    elif element:
+        info = element.text.strip()
+        if info == "Reviews":
+            info = "nd " + info
+    else: 
+        info = "empty" 
+    return info
+#2° -> Sinopse e Titulo
+def separar_sinopse_titulo(filme_arg, class_, element_arg, posicao):
+    info = lista[posicao]
+    element = filme_soup.find(f"{filme_arg}", attrs=f"{class_}")
+    if element:
+        if posicao == 4:
+            text = element.find_all(f"{element_arg}")
+            if len(text) > 1:
+                info = text[1].text.strip()
+        else:
+            text = element.find(f"{element_arg}")
+            if text:
+                info = text.text.strip() 
+    return info
+#1° ->Lancamento, Bilheteria, Genero e Diretor
+def separar_lan_bil_gen_dir():
     lancamento = None
     genero = None
     diretor = None
     bilheteria = None
-    critics_consensu = None
-    audience_consensu = None
-    
     dt_elements = filme_soup.find_all('dt')
     dd_elements = filme_soup.find_all('dd')
     
-    # Buscando a informação dt em dd
     for dt, dd in zip(dt_elements, dd_elements):
-        dt_text = dt.find('rt-text').text.strip() if dt.find('rt-text') else ''
-        dd_text = (dd.find('rt-text').text.strip() if dd.find('rt-text') else '') or \
-                  (dd.find('rt-link').text.strip() if dd.find('rt-link') else '')
+        if dt.find('rt-text'):
+            dt_text = dt.find('rt-text').text.strip()
+        else:
+            dt_text = ''
         
-        # Diferenciando as informações pelo texto que está dentro do elemento dt e armazenando-as
-        if 'Release Date (Theaters)' in dt_text:
-            lancamento = dd_text
-        elif 'Box Office (Gross USA)' in dt_text:
-            bilheteria = dd_text
+        if dd.find('rt-text'):
+            dd_text = dd.find('rt-text').text.strip()
+            if dd.find('rt-link'):
+                dd_text = dd.find('rt-link').text.strip()
+        else:
+            dd_text = ''
             
-    # Mesmo processo do for anterior, porém aqui existem mais de uma informação por dd que estão separadas por links, por isso criamos uma lista 
-    for dt, dd in zip(dt_elements, dd_elements):
-        dt_text = dt.find('rt-text').text.strip() if dt.find('rt-text') else ''
-        dd_links = dd.find_all('rt-link') if dd.find('rt-link') else []
-        dd_texts = [link.text.strip() for link in dd_links]
+        if dd.find('rt-link'):
+            dd_links = dd.find_all('rt-link') 
+        else:
+            dd_links = []
+            
+        for link in dd_links:
+            dd_texts = [link.text.strip()]
         
-        # Novamente fazendo a diferenciação
-        if 'Genre' in dt_text:
-            genero = ', '.join(dd_texts) if dd_texts else None
-        elif 'Director' in dt_text:
-            diretor = ', '.join(dd_texts) if dd_texts else None
+        match dt_text:
+            case 'Release Date (Theaters)': lancamento = dd_text
+            case 'Box Office (Gross USA)': bilheteria = dd_text
+            case 'Genre':
+                if dd_texts:
+                    genero = ', '.join(dd_texts) 
+                else:
+                    genero = None
+            case 'Director':
+                if dd_texts:
+                    diretor = ', '.join(dd_texts) 
+                else:
+                    diretor = None
     
-    # Porcentagem de avaliações dos críticos usando a mesma lógica feita para pegar a sinopse
-    tomatometer_score_element = filme_soup.find('rt-button', attrs={'slot': 'criticsScore'})
-    if tomatometer_score_element:
-        tomatometer_score_text = tomatometer_score_element.find('rt-text')
-        if tomatometer_score_text:
-            tomatometer_score = tomatometer_score_text.text.strip()
-            
-    # Número de avaliações dos críticos usando a mesma lógica feita para pegar a sinopse
-    number_score_text = filme_soup.find('rt-link', attrs={'slot': 'criticsReviews'})
-    if number_score_text:
-        number_score = number_score_text.text.strip()
-            
-    # Porcentagem de avaliações do público usando a mesma lógica feita para pegar a sinopse
-    audience_score_element = filme_soup.find('rt-button', attrs={'slot': 'audienceScore'})
-    if audience_score_element:
-        audience_score_text = audience_score_element.find('rt-text')
-        if audience_score_text:
-            audience_score = audience_score_text.text.strip()
-    
-    # Consenso dos críticos usando a mesma lógica feita para pegar a sinopse
-    critics_consensu_element = filme_soup.find('div', attrs={'id': 'critics-consensus'})
-    if critics_consensu_element:
-        critics_consensu_text = critics_consensu_element.find('p')
-        if critics_consensu_text:
-            critics_consensu = critics_consensu_text.text.strip()
-            
-    # Consenso do público usando a mesma lógica feita para pegar a sinopse
-    audience_consensu_element = filme_soup.find('div', attrs={'id': 'audience-consensus'})
-    if audience_consensu_element:
-        audience_consensu_text = audience_consensu_element.find('p')
-        if audience_consensu_text:
-            audience_consensu = audience_consensu_text.text.strip()
-    
-    # Armazenando nas listas 
-    
-    titulos.append(titulo)
-    sinopses.append(sinopse)
-    lancamentos.append(lancamento)
-    generos.append(genero) 
-    diretores.append(diretor)
-    bilheterias.append(bilheteria)
-    
-    tomatometer_scores.append(tomatometer_score)
-    audience_scores.append(audience_score)
-    number_scores.append(number_score)
-    critics_consensus.append(critics_consensu)
-    audience_consensus.append(audience_consensu)
+    info = [lancamento, bilheteria, genero, diretor]
+    return info
 
-# Mostrar os resultados
-for titulo, sinopse, lancamento, genero, diretor, bilheteria, tomatometer_score, audience_score, number_score, critics_consensu, audience_consensu in zip(titulos, sinopses, lancamentos, generos, diretores, bilheterias, tomatometer_scores, audience_scores, number_scores, critics_consensus, audience_consensus):
-    print(f"Título: {titulo}")
-    print(f"Data de lançamento: {lancamento}")
-    print(f"Gênero: {genero}")
-    print(f"Diretor: {diretor}")
-    print(f"Sinopse: {sinopse}")
-    print(f"Bilheteria (bruto EUA): {bilheteria}")
-    print("-" * 20)
-    print(f"Avaliação de críticos: {tomatometer_score}")
-    print(f"Avaliação do público: {audience_score}")
-    print(f"Número de avaliações de críticos: {number_score}")
-    print(f"Consenso dos críticos: {critics_consensu}")
-    print(f"Consenso do público: {audience_consensu}")
-    print("-" * 20)
-    print(" ")
+#Trancrevendo os dados para confirmação (opcional)
+def escrever_filmes(lista, listas):
+    i = 0
+    while i < len(listas):  
+        print(f"Título: {lista[0][i]}")
+        print(f"Data de lançamento: {lista[1][i]}")
+        print(f"Gênero: {lista[2][i]}")
+        print(f"Diretor: {lista[3][i]}")
+        print(f"Sinopse: {lista[4][i]}")
+        print(f"Bilheteria (bruto EUA): {lista[5][i]}")
+        print("-" * 20)
+        print(f"Avaliação de críticos: {lista[6][i]}")
+        print(f"Avaliação do público: {lista[7][i]}")
+        print(f"Número de avaliações de críticos: {lista[8][i]}")
+        print(f"Consenso dos críticos: {lista[9][i]}")
+        print(f"Consenso do público: {lista[10][i]}")
+        print("-" * 20)
+        print(" ")
+        i += 1
+
+#gerando as funções
+def call_main():
+    global soup, lista, filme_soup
+    url = "https://www.rottentomatoes.com/browse/movies_in_theaters/sort:popular"
+    (soup) = scraping(url)
+    lista = declarar_listas()
+    for link_filme in encontrar_link():
+        filme_url = "https://www.rottentomatoes.com" + link_filme
+        filme_soup = scraping(filme_url)
+
+        titulo = None
+        sinopse = None
+        critics_consensu = None
+        audience_consensu = None
+        tomatometer_score = None
+        audience_score = None
+        number_score = None
+
+        #chamamando cada tipo de informação separados
+        titulo = separar_sinopse_titulo("h1", "unset", "span", 0)
+        lancamento = separar_lan_bil_gen_dir()[0]
+        bilheteria = separar_lan_bil_gen_dir()[1]
+        genero = separar_lan_bil_gen_dir()[2]
+        diretor =separar_lan_bil_gen_dir()[3]
+        sinopse = separar_sinopse_titulo("div", "synopsis-wrap", "rt-text", 4)
+        tomatometer_score = separar_critics_audience("rt-button", "slot", "criticsScore", "rt-text", 6)
+        audience_score = separar_critics_audience("rt-button", "slot", "audienceScore", "rt-text", 7)
+        number_score = separar_critics_audience("rt-link", "slot", "criticsReviews", "", 8)
+        critics_consensu = separar_critics_audience("div", "id", "critics-consensus", "p", 9)
+        audience_consensu = separar_critics_audience("div", "id", "audience-consensus", "p", 10)
+        
+        # Armazenando nas listas 
+        lista[0].append(titulo)
+        lista[1].append(lancamento)
+        lista[2].append(genero) 
+        lista[3].append(diretor)
+        lista[4].append(sinopse)
+        lista[5].append(bilheteria)
+        lista[6].append(tomatometer_score)
+        lista[7].append(audience_score)
+        lista[8].append(number_score)
+        lista[9].append(critics_consensu)
+        lista[10].append(audience_consensu)
+        
+    listas = declarar_listas()
+    escrever_filmes(lista, listas)
+
+#chamando a main
+call_main()
