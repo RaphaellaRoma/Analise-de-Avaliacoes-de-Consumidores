f94c18f3afaeaa57cb93789184cd3e85685a356d
Maredol <b435924@fgv.edu.br>
2024-06-24 21:15:18 -0300

    desenvolvimento inicial do arq main.py, nele atualmente são chamados apenas o gerar_trope do trope_prompt.py e lista do web_scraping.py ambos são utilizados para melhorar a filtragem de tropes usando a função most_frequent no arq também existe a função escrever_tropes para auxiliar o desenvolvimento do main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from trope_prompt import gerar_trope
+from web_scraping import lista
+
+def most_frequent():
+    n = 0
+    i = 0
+    tropes = []
+    main_tropes = []
+    
+    while n < len(lista[0]):
+        while i < 20:
+            trope = gerar_trope(lista[4][n], lista[9][n], lista[10][n])
+            tropes.append(trope)
+            if i == 19:
+                main_trope = max(set(tropes), key = tropes.count)
+                main_tropes.append(main_trope)
+            i += 1
+    
+        i = 0
+        tropes.clear()
+        n += 1 
+    return main_tropes
+
+def escrever_tropes(lista, trope):
+    i = 0
+    while i < len(lista[0]):  
+        print(f"Filme: {lista[0][i]}")
+        print(f"Trope: {trope[i]}")
+        print("-" * 20)
+        print(" ")
+        i += 1
+        
+trope = most_frequent()
+#escrever_tropes(lista, trope)
