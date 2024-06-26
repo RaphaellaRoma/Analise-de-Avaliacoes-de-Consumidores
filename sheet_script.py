d10c3876f513100ff8035b1a7e2919881c92567d
ErikRolinFGV <erik7nonato@gmail.com>
2024-06-25 21:55:09 -0300

    primeira tentativa de mandar a sheet Tabela de Filmes para o arq analise_de_filmes.xlsx mudança nas posições das rows e cols para auxiliar no desenvolvimento da planilha

--- sheet_script.py
+++ sheet_script.py
@@ -1,10 +1,9 @@
-from openpyxl import Workbook
+from openpyxl import load_workbook
 from web_scraping import lista, declarar_listas
 from main import trope
-import openpyxl.styles
 
 #incializando os excel
-wb = Workbook()
+wb = load_workbook('analise_de_filmes.xlsx')
 ws = wb.active
 ws.title = "Tabela de Filmes"
 
@@ -28,33 +27,33 @@ names =[
 for n in range(len(declarar_listas())):
     while i < (len(lista[0])):
         if n == 4 or n == 9 or n == 10 or n == 11:
-            ws.cell(row=i+3, column=n+2)
+            ws.cell(row=i+1, column=n+1)
         else:
-            ws.cell(row=i+3, column=n+2, value=lista[n][i])
+            ws.cell(row=i+1, column=n+1, value=lista[n][i])
         i += 1
     i = 0
     n += 1
 
 #deletando colunas vazias
-ws.delete_cols(11,3)
-ws.delete_cols(6,1)
+ws.delete_cols(10,3)
+ws.delete_cols(5,1)
 
 #inserção dos nomes para a primeira linha
 i = 0
 while i < len(names):
-    ws.cell(row=2, column=i+2, value=names[i])
+    ws.cell(row=1, column=i+1, value=names[i])
     i += 1
 
 #criação da coluna 5
-ws.insert_cols(5, 1)
+ws.insert_cols(4, 1)
 #inserção do nome "Trope" para a primeira linha da coluna 5
-ws.cell(row=2, column=5, value="Trope")
+ws.cell(row=1, column=4, value="Trope")
 
 #inserção de dados dos tropes para a coluna 5
 i = 0
 while i < len(trope):
-    ws.cell(row=i+3, column=5, value=(trope[i].replace("trope", "")))
+    ws.cell(row=i+2, column=4, value=(trope[i].replace("trope", "")))
     i += 1
 
 #salvando a tabela no excel
-wb.save("rotten_tomatos.xlsx")
+wb.save('analise_de_filmes.xlsx')
